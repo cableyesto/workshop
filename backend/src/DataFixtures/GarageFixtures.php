4947d6559970e9b8f3a47d1ddc1aef77e00396a1
cableyesto <dialkins@tutamail.com>
2026-04-10 00:29:34 +0200

    feat: Create the garage Entity and fixtures. Add quality tools. Create dump controller.

---
+++ backend/src/DataFixtures/GarageFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Garage;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class GarageFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 5; ++$i) {
+            $garage = new Garage();
+
+            $garage->setSIRETNumber($faker->unique()->numerify('##############'))
+                ->setName($faker->company())
+                ->setStreet($faker->streetAddress())
+                ->setCity($faker->city())
+                ->setZipCode($faker->postcode())
+                ->setPhone($faker->phoneNumber());
+
+            $manager->persist($garage);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ backend/src/Entity/Garage.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GarageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GarageRepository::class)]
+class Garage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 14, unique: true)]
+    private ?string $SIRET_number = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $street = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 5)]
+    private ?string $zip_code = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $phone = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSIRETNumber(): ?string
+    {
+        return $this->SIRET_number;
+    }
+
+    public function setSIRETNumber(string $SIRET_number): static
+    {
+        $this->SIRET_number = $SIRET_number;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): static
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(string $zip_code): static
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+}
