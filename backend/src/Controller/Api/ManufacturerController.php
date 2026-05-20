604e8343245ce73c2b1e89012cc32296f900db15
cableyesto <dialkins@tutamail.com>
2026-05-20 13:36:04 +0200

    feat: create the manufacturer controller.

---
+++ backend/src/Controller/Api/ManufacturerController.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Repository\ManufacturerRepository;
+use Illuminate\Support\Collection;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ManufacturerController
+{
+    public function __construct(
+        private readonly ManufacturerRepository $manufacturerRepository,
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    #[Route('/api/manufacturers', name: 'api_manufacturers_index', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        $this->logger->info('Fetching all manufacturers');
+
+        $manufacturers = $this->manufacturerRepository->findAll();
+
+        $data = Collection::make($manufacturers)
+            ->map(fn($manufacturer) => [
+                'id' => $manufacturer->getId(),
+                'name' => $manufacturer->getName(),
+            ])
+            ->sortBy('name')
+            ->values()
+            ->toArray();
+
+        return new JsonResponse($data);
+    }
+}
