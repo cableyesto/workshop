e97f53a3b677b17413e54d42ced551801d6e8636
cableyesto <dialkins@tutamail.com>
2026-07-13 07:41:15 +0200

    frontend: create the landing page of the application website.

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Liip\MonitorBundle\LiipMonitorBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
